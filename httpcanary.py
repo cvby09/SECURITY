d391d7f22dc035f8b42559e93d8689e25adae853
cvby09 <zack.smith.w@gmail.com>
2024-07-12 17:52:31 +0600

    Create httpcanary.py

---
+++ httpcanary.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+#!/coded
+
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'com.httpcanary.pro')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+except:
+    pass
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'com.guoshi.httpcanary')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+except:
+    pass
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'com.wasim')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+except:
+    pass
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'com.purple.canary')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+
+except:
+    pass
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'com.guoshi.httpcanary.premium')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+except:
+    pass
+try:
+    if os.path.exists(os.path.join('/sdcard/Android/data', 'app.greyshirts.sslcapture')):
+        print('\n\x1b[38;5;204m[ðŸ˜ˆ] FIRST UNINSTALL THE HTTP CANARY\n')
+        exit()
+    else:
+        pass
+except:
+    pass
